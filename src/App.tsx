796dcae7771cce747e74f6a3ca2e25febeee3530
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-07-13 17:43:27 +0900

    2 px

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import './cardStyle.css';
 function App() {
   return (
     <div className='main-container'>
-      <div className='card-1'>
-        <span className='number-2'>2</span>
-        <div className='flex-column-cbd'>
-          <span className='circle'>♥</span>
-          <span className='circle-2'>♥</span>
-        </div>
-        <span className='number-2-3'>2</span>
+    <div className='card-1'>
+      <span className='number-2'>2</span>
+      <div className='flex-column-cbd'>
+        <span className='circle'>◯</span>
+        <span className='text-3'>◯</span>
       </div>
+      <span className='text-4'>2</span>
     </div>
+  </div>
   );
 }
 
