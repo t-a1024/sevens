e433c3d3c0ff9be6bb65cb0ac777c65cd77c09fb
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-07-13 15:43:40 +0900

    ハートのA

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import './cardStyle.css';
+
 function App() {
   return (
     <div className="App">
+      <div className='main-container'>
+      <div className='card'>
+        <span className='span-a'>A</span>
+        <span className='span-circle'>♥</span>
+        <span className='span-a-1'>A</span>
+      </div>
+    </div>
     </div>
   );
 }
