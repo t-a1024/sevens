22e216e9befe09a961feafb9196a7f558d0591fb
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-08-07 17:10:20 +0900

    並び替え

--- src/App.tsx
+++ src/App.tsx
@@ -15,28 +15,24 @@ function App() {
   // CardDataからidが0のカードを取り除く
   const filteredCardData = CardData.filter(card => card.id !== 0);
 
-  // CardDataとmarkDataの組み合わせを作成
-  const combinedData = filteredCardData.flatMap(card =>
-    markData.map(mark => ({
+  const combinedData = markData.flatMap(mark =>
+    filteredCardData.map(card => ({
       ...card,
-      mark: mark.label,
-      color: mark.color
+      mark:mark.label,
+      color:mark.color
     }))
   );
 
-  // シャッフルされたカードデータ
-  const shuffledData = shuffleArray(combinedData);
 
   return (
     <div className="App">
-      {/* <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {shuffledData.map((card, index) => (
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {combinedData.map((card, index) => (
           <div key={index} style={{ margin: "10px" }}>
             <Card id={card.id} mark={card.mark} color={card.color} />
           </div>
         ))}
-      </div> */}
-      <Card id={10} mark={'♠'} color={'red'}></Card>
+      </div>
     </div>
   );
 }
